0c67485e97c88573835d9e1a50975dfd40e30bd4
ihonsdu <erbolat_j_92@mail.ru>
2013-07-13 03:29:55 +0600

    Only first page

--- ENTapp/src/main/java/com/isg/entapp/MainActivity.java
+++ ENTapp/src/main/java/com/isg/entapp/MainActivity.java
@@ -1,15 +1,30 @@
 package com.isg.entapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements View.OnClickListener {
+            Button kaz,rus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        kaz = (Button) findViewById(R.id.kk_button);
+        rus = (Button) findViewById(R.id.ru_button);
+        kaz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.findViewById(R.id.kk_button);
+                Intent subject;
+                subject = new Intent(MainActivity.this, subjects.class);
+                startActivity(subject);
+            }
+        });
     }
 
 
@@ -19,5 +34,10 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
 }
