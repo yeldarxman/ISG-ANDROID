36560905cae5aa1aaa6870fcf4eca0c07d424ce1
yeldarxman <merkaziyev@gmail.com>
2013-07-13 18:03:20 +0100

    - quick fixes

--- ENTapp/src/main/java/com/isg/entapp/Fragments/Test/TestSubjectSelection.java
+++ ENTapp/src/main/java/com/isg/entapp/Fragments/Test/TestSubjectSelection.java
@@ -45,16 +45,6 @@ public class TestSubjectSelection extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, final View view,
                                     int position, long id) {
-                final String item = (String) parent.getItemAtPosition(position);
-                view.animate().setDuration(2000).alpha(0)
-                        .withEndAction(new Runnable() {
-                            @Override
-                            public void run() {
-                                list.remove(item);
-                                adapter.notifyDataSetChanged();
-                                view.setAlpha(1);
-                            }
-                        });
             }
 
         });
@@ -65,30 +55,5 @@ public class TestSubjectSelection extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.test_subject_selection_fragment, container, false);
-    }
-
-    private class StableArrayAdapter extends ArrayAdapter<String> {
-
-        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
-
-        public StableArrayAdapter(Context context, int textViewResourceId,
-                                  List<String> objects) {
-            super(context, textViewResourceId, objects);
-            for (int i = 0; i < objects.size(); ++i) {
-                mIdMap.put(objects.get(i), i);
-            }
-        }
-
-        @Override
-        public long getItemId(int position) {
-            String item = getItem(position);
-            return mIdMap.get(item);
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
     }
 }
