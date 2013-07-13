ba7238e60472413a4a8d9b2ed145a32f5af9aa39
yeldarxman <merkaziyev@gmail.com>
2013-07-13 14:25:20 +0100

    Added few files

---
+++ ENTapp/src/main/java/com/isg/entapp/Fragments/Test/TestSubjectSelection.java
@@ -0,0 +1,31 @@
+package com.isg.entapp.Fragments.Test;
+
+/**
+ * Created by yeldar on 13.07.13.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.isg.entapp.R;
+
+public class TestSubjectSelection extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (container == null) {
+            // We have different layouts, and in one of them this
+            // fragment's containing frame doesn't exist.  The fragment
+            // may still be created from its saved state, but there is
+            // no reason to try to create its view hierarchy because it
+            // won't be displayed.  Note this is not needed -- we could
+            // just run the code below, where we would create and return
+            // the view hierarchy; it would just never be used.
+            return null;
+        }
+        return (LinearLayout)inflater.inflate(R.layout.test_subject_selection_fragment, container, false);
+    }
+}
