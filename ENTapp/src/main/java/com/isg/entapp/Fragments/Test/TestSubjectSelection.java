8dc0b8e82986970ead91c1c33b68dee5af1ab6d3
yeldarxman <merkaziyev@gmail.com>
2013-07-14 18:33:14 +0100

    Integrated ORMlite, however still need to improve it

--- ENTapp/src/main/java/com/isg/entapp/Fragments/Test/TestSubjectSelection.java
+++ ENTapp/src/main/java/com/isg/entapp/Fragments/Test/TestSubjectSelection.java
@@ -13,31 +13,30 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.sql.SQLException;
 import java.util.*;
 
 import com.isg.entapp.ListAdapters.Test.TestSubjectSelectionListAdapter;
+import com.isg.entapp.Models.Subject;
+import com.isg.entapp.Models.Test;
+import com.isg.entapp.Models.Topic;
 import com.isg.entapp.R;
+import com.isg.entapp.Utilities.Database;
 
 public class TestSubjectSelection extends Fragment {
+    private Database database;
+    private Subject[] subjects;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         final ListView listview = (ListView) getActivity().findViewById(R.id.listview);
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-                "Android", "iPhone", "WindowsMobile" };
 
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
-        }
-
+        //load the subject objects
+        this.loadSubjects();
 
         //final StableArrayAdapter adapter = new StableArrayAdapter(getActivity(), R.layout.test_subject_list_item, list);
-        final TestSubjectSelectionListAdapter adapter = new TestSubjectSelectionListAdapter(getActivity(), values);
+        final TestSubjectSelectionListAdapter adapter = new TestSubjectSelectionListAdapter(getActivity(), subjects);
         listview.setAdapter(adapter);
 
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -55,5 +54,63 @@ public class TestSubjectSelection extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.test_subject_selection_fragment, container, false);
+    }
+
+    public void loadSubjects(){
+        //initialize the database object
+        database = new Database(getActivity());
+
+        // if there are no subjects in the database then create them
+        if(database.getAllSubjects().size() == 0){
+
+            System.out.println("Creating subjects from scratch");
+
+            subjects = new Subject[11];
+
+            for(int i=0; i<11; i++){
+                subjects[i] = new Subject();
+                database.addSubject(subjects[i]);
+            }
+
+            subjects[0].setName("Math");
+            subjects[1].setName("Kaz history");
+            subjects[2].setName("Kaz language");
+            subjects[3].setName("Rus language");
+            subjects[4].setName("Eng language");
+            subjects[5].setName("Physics");
+            subjects[6].setName("Chemistry");
+            subjects[7].setName("Biology");
+            subjects[8].setName("Geography");
+            subjects[9].setName("World history");
+            subjects[10].setName("Kaz literature");
+
+
+            for(int i=0; i<11; i++){
+                Topic variant = new Topic(subjects[i], true, "Variants");
+                database.addTopic(variant);
+
+                for(int k=0; k<15; k++){
+                    database.addTest(new Test("Test" + (k+1), variant));
+                }
+
+                for (int j=0; j<5; j++){
+                    Topic topic = new Topic(subjects[i], false, "Topic" + (j+1));
+                    database.addTopic(topic);
+
+                    for(int k=0; k<15; k++){
+                        database.addTest(new Test("Test" + (k+1), topic));
+                    }
+                }
+            }
+
+            for(int i=0; i<11; i++){
+                database.subjectRuntimeDAO().update(subjects[i]);
+            }
+
+        } else {
+            System.out.println("Loading subjects from database");
+            subjects = new Subject[11];
+            subjects = database.getAllSubjects().toArray(subjects);
+        }
     }
 }
